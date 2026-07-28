836ce7a9bc008b871d180250e9b58e1e7d24dc57
James Alvarez <mail@jamesalvarez.co.uk>
2026-07-28 13:04:27 +0100

    Restrict dev site to authenticated users with placeholder homepage

--- routes/web.php
+++ routes/web.php
@@ -9,26 +9,24 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-// Public routes
-Route::get('/search', SearchController::class)->name('search');
-
-Route::get('/individuals', [IndividualController::class, 'index'])->name('individuals.index');
-Route::get('/individuals/{individual}', [IndividualController::class, 'show'])->name('individuals.show');
-
-Route::get('/holdings', [HoldingController::class, 'index'])->name('holdings.index');
-Route::get('/holdings/{holding}', [HoldingController::class, 'show'])->name('holdings.show');
-
-Route::get('/parishes', [ParishController::class, 'index'])->name('parishes.index');
-Route::get('/parishes/{parish}', [ParishController::class, 'show'])->name('parishes.show');
-
-// Authenticated routes
 Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::get('/search', SearchController::class)->name('search');
+
+    Route::get('/individuals', [IndividualController::class, 'index'])->name('individuals.index');
+    Route::get('/individuals/{individual}', [IndividualController::class, 'show'])->name('individuals.show');
+
+    Route::get('/holdings', [HoldingController::class, 'index'])->name('holdings.index');
+    Route::get('/holdings/{holding}', [HoldingController::class, 'show'])->name('holdings.show');
+
+    Route::get('/parishes', [ParishController::class, 'index'])->name('parishes.index');
+    Route::get('/parishes/{parish}', [ParishController::class, 'show'])->name('parishes.show');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
