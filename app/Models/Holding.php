2cf7f27d35c4167b0b15d183751b2e2b94647f55
James Alvarez <mail@jamesalvarez.co.uk>
2026-07-28 10:19:46 +0100

    Add convenience relationships and annotation morphs to Individual and Holding

--- app/Models/Holding.php
+++ app/Models/Holding.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Laravel\Scout\Searchable;
 
 class Holding extends Model
@@ -52,5 +53,10 @@ class Holding extends Model
     public function holdingEstateLinks(): HasMany
     {
         return $this->hasMany(HoldingEstateLink::class);
+    }
+
+    public function annotations(): MorphMany
+    {
+        return $this->morphMany(RecordAnnotation::class, 'annotatable');
     }
 }
--- app/Models/Individual.php
+++ app/Models/Individual.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Laravel\Scout\Searchable;
 
 class Individual extends Model
@@ -54,6 +56,16 @@ class Individual extends Model
         return $this->hasMany(EnslaverMatch::class);
     }
 
+    public function enslavedRecords(): HasManyThrough
+    {
+        return $this->hasManyThrough(EnslavedRecord::class, EnslavedMatch::class, 'individual_id', 'id', 'id', 'enslaved_record_id');
+    }
+
+    public function enslaverRecords(): HasManyThrough
+    {
+        return $this->hasManyThrough(EnslaverRecord::class, EnslaverMatch::class, 'individual_id', 'id', 'id', 'enslaver_record_id');
+    }
+
     public function relationshipsAsSubject(): HasMany
     {
         return $this->hasMany(Relationship::class, 'person1_id');
@@ -62,5 +74,20 @@ class Individual extends Model
     public function relationshipsAsRelated(): HasMany
     {
         return $this->hasMany(Relationship::class, 'person2_id');
+    }
+
+    public function annotations(): MorphMany
+    {
+        return $this->morphMany(RecordAnnotation::class, 'annotatable');
+    }
+
+    public function isEnslaved(): bool
+    {
+        return $this->enslavedMatches()->exists();
+    }
+
+    public function isEnslaver(): bool
+    {
+        return $this->enslaverMatches()->exists();
     }
 }
