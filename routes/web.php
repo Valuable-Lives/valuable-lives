446ccf76209612e790e2e41fc1d05dd154e0c1b3
James Alvarez <mail@jamesalvarez.co.uk>
2026-07-28 10:21:35 +0100

    Wire up routes for individuals, holdings, parishes, and search

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\HoldingController;
+use App\Http\Controllers\IndividualController;
+use App\Http\Controllers\ParishController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SearchController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -11,6 +15,19 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// Public routes
+Route::get('/search', SearchController::class)->name('search');
+
+Route::get('/individuals', [IndividualController::class, 'index'])->name('individuals.index');
+Route::get('/individuals/{individual}', [IndividualController::class, 'show'])->name('individuals.show');
+
+Route::get('/holdings', [HoldingController::class, 'index'])->name('holdings.index');
+Route::get('/holdings/{holding}', [HoldingController::class, 'show'])->name('holdings.show');
+
+Route::get('/parishes', [ParishController::class, 'index'])->name('parishes.index');
+Route::get('/parishes/{parish}', [ParishController::class, 'show'])->name('parishes.show');
+
+// Authenticated routes
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
